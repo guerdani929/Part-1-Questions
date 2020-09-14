fb0245286ed491bf0235364a388992df09bb45b0
Daniel Guernsey <GuerDani929@GOTVDSB.CA>
2020-09-14 13:31:04 -0400

    Add project files.

---
+++ Part 1 Questions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("DDDD    GGG ");
+            Console.WriteLine("D   D  G   G");
+            Console.WriteLine("D   D  G ");
+            Console.WriteLine("D   D  GGGGG");
+            Console.WriteLine("D   D  G   G");
+            Console.WriteLine("D   D  G   G ");
+            Console.WriteLine("DDDD    GGG ");
+
+
+            string firstName = "Daniel";
+            string favMovie = "The Matrix";
+
+            firstName = firstName.ToLower();
+            favMovie = favMovie.ToLower();
+
+            Console.WriteLine($"Greetings {firstName} is your favourite movie {favMovie}?".ToLower());
+
+            favMovie = favMovie.ToUpper();
+            Console.WriteLine(favMovie);
+
+            Console.WriteLine(favMovie.Contains("THE"));
+
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+
+            Console.WriteLine(favMovie);
+
+       
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
